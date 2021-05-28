97d2cf890252a2e400ab0410ae68300e57d172ff
fmontiel2019145 <69689864+fmontiel2019145@users.noreply.github.com>
2021-05-28 00:08:01 -0600

    Add files via upload

---
+++ angular/src/app/models/hotels.list.ts
@@ -0,0 +1,12 @@
+export interface Hotels{
+    _id : string,
+    nombreHotel : string,
+    descripcionHotel : string,
+    direccionHotel : string,
+    paisHotel : string,
+    cpHotel : string,
+    pbxHotel : number,
+    calificacionesHotel : any,
+    habitacionesHotel : any,
+    eventosHotel : any
+}
